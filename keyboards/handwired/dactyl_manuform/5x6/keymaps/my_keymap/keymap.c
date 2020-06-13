df9f97e56d314204689d607359ec9b4053bd3ba7
m <1315979+mattse@users.noreply.github.com>
2020-06-13 00:20:08 -0400

    keyboard for testing

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/keymap.c
@@ -14,13 +14,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
      KC_GRAVE , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINUS,
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
-     KC_ESCAPE, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     KC_LSHIFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSHIFT,
-                      KC_CAPSLOCK,KC_LBRACKET,                                                       KC_RBRACKET, KC_EQL,
-                                      KC_LALT,KC_SPC,                        KC_BSPACE, KC_ENTER,
-                                      KC_LCTRL,KC_PGUP,                         KC_AUDIO_VOL_UP,  DF(1),
-                                      KC_LGUI, KC_PGDOWN,                        KC_AUDIO_VOL_DOWN, DF(2)
+     KC_F1 , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSLASH,
+     KC_F2, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+     KC_F3, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_KP_7,
+                      KC_F4,KC_LBRACKET,                                                       KC_RBRACKET, KC_EQL,
+                                      KC_F5,KC_SPC,                        KC_KP_6, KC_KP_1,
+                                      KC_F6,KC_F8,                         KC_KP_8,  KC_KP_4,
+                                      KC_F7, KC_F9,                        KC_KP_2, KC_KP_5
   ),
 
   [_LOWER] = LAYOUT_5x6(
