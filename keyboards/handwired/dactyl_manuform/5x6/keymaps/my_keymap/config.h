898962a9fecb1f3046474686f40222fffe57e8ea
Matthew Tse <1315979+m-tse@users.noreply.github.com>
2020-06-20 09:46:28 -0400

    Swap rows and columns due to my wiring preference

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/config.h
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/my_keymap/config.h
@@ -29,12 +29,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 // wiring of each half, use the bottom left 6 and the bottom right 6 pins.
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { F6, F7, B1, B3, B2, B6 }
+#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4, B5 }
 
 // Mirror the pins on the other side.
-#define MATRIX_COL_PINS_RIGHT { F6, F7, B1, B3, B2, B6 }
-#define MATRIX_ROW_PINS_RIGHT { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS_RIGHT { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS_RIGHT { F6, F7, B1, B3, B2, B6 }
 
 // Default diode direction
 #define DIODE_DIRECTION COL2ROW
